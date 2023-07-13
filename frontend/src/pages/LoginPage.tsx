92bc14b1c4bfc29b484aefb9bcbc19746564d0a3
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-07-13 23:34:06 +0200

    [Frontend] Add some color variables in variable file

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -6,6 +6,17 @@ export default function LoginPage() {
     <div>
       <img src={logo} alt="logo" />
       <h2 className={`${style.title}`}>Se connecter</h2>
+      <p>Vous n'avez pas de compte ? Créer un compte</p>
+      <form>
+        <label>
+          Adresse email*
+          <input type="email" />
+        </label>
+        <label>
+          Mot de passe*
+          <input type="password" />
+        </label>
+      </form>
     </div>
   );
 }
