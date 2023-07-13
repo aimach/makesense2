0f59bde7b4d91bde3a5ed9deaf8b43ac039b0bf8
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-07-13 23:25:10 +0200

    [Frontend] Add Sass

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,11 @@
 import logo from "../assets/img/logo.svg";
+import style from "./LoginPage.module.scss";
 
 export default function LoginPage() {
   return (
     <div>
       <img src={logo} alt="logo" />
+      <h2 className={`${style.title}`}>Se connecter</h2>
     </div>
   );
 }
