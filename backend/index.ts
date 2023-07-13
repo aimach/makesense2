6f48e3bae38c0adc8d5fdc0bb9b161204ec46cbf
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-07-13 22:34:05 +0200

    [backend] add eslint for code quality improvement

--- backend/index.ts
+++ backend/index.ts
@@ -1,24 +1,27 @@
-const { PrismaClient } = require("@prisma/client");
-const express = require("express");
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import express from "express";
 const app = express();
 const port = 5000;
 const prisma = new PrismaClient();
 
 app.use(express.json());
 
-app.get("/", async (req: any, res: any) => {
+app.get("/", async (req: Request, res: Response) => {
   const services = await prisma.service.findMany();
   res.json(services);
 });
 
-app.post("/service", async (req: any, res: any) => {
+app.post("/service", async (req: Request, res: Response) => {
   const { name } = req.body;
   const service = await prisma.service.create({
     data: {
       name,
     },
   });
-  res.json(service);
+  if (service) {
+    res.json(service);
+  }
 });
 
 app.listen(port, () => {
