9c16460a09ad4ddba6f4b8bb0b4253fef8ce5bbb
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-07-19 20:11:45 +0200

    add category controllers

---
+++ backend/src/controller/categoryControllers.ts
@@ -0,0 +1,86 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+export const categoryControllers = {
+  // READ
+  getAllCategories: async (req: Request, res: Response): Promise<void> => {
+    try {
+      const allCategories = await prisma.category.findMany();
+      res.status(200).send(allCategories);
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({ error: "Error while reading categories" });
+    }
+  },
+  getCategoryById: async (req: Request, res: Response): Promise<void> => {
+    try {
+      const categoryToRead = await prisma.category.findUnique({
+        where: {
+          id: parseInt(req.params.id),
+        },
+      });
+      if (categoryToRead === null) {
+        res.status(404).send({ error: "Category not found" });
+        return;
+      }
+      res.status(200).send(categoryToRead);
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({ error: "Error while reading category" });
+    }
+  },
+
+  // CREATE
+  createCategory: async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { name, color } = req.body;
+      await prisma.category.create({
+        data: {
+          name,
+          color,
+        },
+      });
+      res.status(201).send("Created category");
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({ error: "Error while creating category" });
+    }
+  },
+
+  // UPDATE
+  updateCategory: async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { name, color } = req.body;
+      await prisma.category.update({
+        where: {
+          id: parseInt(req.params.id),
+        },
+        data: {
+          name,
+          color,
+        },
+      });
+      res.status(201).send("Updated category");
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({ error: "Error while updating category" });
+    }
+  },
+
+  // UPDATE
+  deleteCategory: async (req: Request, res: Response): Promise<void> => {
+    try {
+      await prisma.category.delete({
+        where: {
+          id: parseInt(req.params.id),
+        },
+      });
+      res.status(201).send("Deleted category");
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({ error: "Error while deleting category" });
+    }
+  },
+};
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -3,6 +3,7 @@ export const router = express.Router();
 
 import { serviceControllers } from "./controller/serviceControllers";
 import { groupControllers } from "./controller/groupControllers";
+import { categoryControllers } from "./controller/categoryControllers";
 // SERVICES
 
 router.get("/services", serviceControllers.getAllServices);
@@ -18,3 +19,11 @@ router.get("/groups/:id", groupControllers.getGroupById);
 router.post("/groups", groupControllers.createGroup);
 router.put("/groups/:id", groupControllers.updateGroup);
 router.delete("/groups/:id", groupControllers.deleteGroup);
+
+// CATEGORIES
+
+router.get("/categories", categoryControllers.getAllCategories);
+router.get("/categories/:id", categoryControllers.getCategoryById);
+router.post("/categories", categoryControllers.createCategory);
+router.put("/categories/:id", categoryControllers.updateCategory);
+router.delete("/categories/:id", categoryControllers.deleteCategory);
