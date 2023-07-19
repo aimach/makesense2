fcfbdac0b0b37279605cf33bb4f83b89bd8e83a9
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-07-19 10:24:50 +0200

    add flex to header and menu

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -13,30 +13,32 @@ export default function Navbar() {
       <Link to="/home">
         <img src={logo} alt="logo" width={"100px"} />
       </Link>
-      <nav>
-        <ul>
-          <li>
-            <Home className={style.icon} />
-            Parcourir les décisions
-          </li>
-          <li>
-            <Calendar className={style.icon} />
-            Mes décisions
-          </li>
-          <li>
-            <Smile className={style.icon} />
-            Notifications
-          </li>
-        </ul>
-      </nav>
-      <button
-        className={style.userMenuButton}
-        onClick={() => setOpenModale(!openModale)}
-      >
-        <Menu />
-        <User className={style.userMenuButtonUserIcon} />
-      </button>
-      {openModale && <UserMenuModale />}
+      <div className={style.headerMenu}>
+        <nav>
+          <ul>
+            <li>
+              <Home className={style.icon} />
+              <p>Parcourir les décisions</p>
+            </li>
+            <li>
+              <Calendar className={style.icon} />
+              <p>Mes décisions</p>
+            </li>
+            <li>
+              <Smile className={style.icon} />
+              <p>Notifications</p>
+            </li>
+          </ul>
+        </nav>
+        <button
+          className={style.userMenuButton}
+          onClick={() => setOpenModale(!openModale)}
+        >
+          <Menu />
+          <User className={style.userMenuButtonUserIcon} />
+        </button>
+        {openModale && <UserMenuModale />}
+      </div>
     </header>
   );
 }
