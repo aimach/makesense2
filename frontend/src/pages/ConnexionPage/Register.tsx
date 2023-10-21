c8853a94288e8244aa10723f2acdfb23cbd42d15
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-10-21 16:50:51 +0200

    add regex file with regex for email

--- frontend/src/pages/ConnexionPage/Register.tsx
+++ frontend/src/pages/ConnexionPage/Register.tsx
@@ -1,8 +1,18 @@
 import { connexionPropType } from "./Login";
 import style from "./ConnexionPage.module.scss";
 import { HelpCircle } from "react-feather";
+import regex from "../../utils/regex";
 
 export default function Register({ setConnexionType }: connexionPropType) {
+  // VALIDATE DATA WITH REGEX
+  const verifyData = (e, dataType) => {
+    if (e.target.value.match(dataType) || e.target.value == "") {
+      setRequest({ ...request, [e.target.name]: e.target.value });
+      setErrors({ ...errors, [e.target.name]: false });
+    } else {
+      setErrors({ ...errors, [e.target.name]: true });
+    }
+  };
   return (
     <div>
       <p>
@@ -18,7 +28,11 @@ export default function Register({ setConnexionType }: connexionPropType) {
         <label>
           Adresse email * <HelpCircle className={style.helpIcon} />
         </label>
-        <input type="email" className={style.inputStyle} />
+        <input
+          type="email"
+          className={style.inputStyle}
+          onChange={(e) => verifyData(e, regex[name])}
+        />
         <label>
           Mot de passe * <HelpCircle className={style.helpIcon} />
         </label>
---
+++ frontend/src/utils/regex.ts
@@ -0,0 +1,5 @@
+const regex = {
+  email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+};
+
+export default regex;
