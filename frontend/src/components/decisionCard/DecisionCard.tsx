a474fe619bd21f1adc3d4892124057b9c579982b
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-12-04 11:10:18 +0100

    feature : onclik on decision card, display decision page

--- frontend/src/components/decisionCard/DecisionCard.tsx
+++ frontend/src/components/decisionCard/DecisionCard.tsx
@@ -1,6 +1,7 @@
 import style from "./DecisionCard.module.scss";
 import { CategoryType, DecisionType } from "../../utils/types";
 import Tag from "../tag/Tag";
+import { Link } from "react-router-dom";
 interface DecisionCardProps {
   decision: DecisionType;
 }
@@ -13,21 +14,23 @@ export default function DecisionCard({ decision }: DecisionCardProps) {
       .slice(0, 35 * 4)
       .join("") + "...";
   return (
-    <div className={style.decisionCard}>
-      <div className={style.titleSection}>
-        <h4>{decision.title}</h4>
-        <img src={decision.user.avatar} alt="avatar" />
+    <Link to={`/decisions/${decision.id}`}>
+      <div className={style.decisionCard}>
+        <div className={style.titleSection}>
+          <h4>{decision.title}</h4>
+          <img src={decision.user.avatar as string} alt="avatar" />
+        </div>
+        <p>{descriptionSliced}</p>
+        <div className={style.tagContainer}>
+          {decision.categories.map((category: CategoryType) => (
+            <Tag
+              content={category.category.name}
+              color={category.category.color}
+              key={category.category.id}
+            />
+          ))}
+        </div>
       </div>
-      <p>{descriptionSliced}</p>
-      <div className={style.tagContainer}>
-        {decision.categories.map((category: CategoryType) => (
-          <Tag
-            content={category.category.name}
-            color={category.category.color}
-            key={category.category.id}
-          />
-        ))}
-      </div>
-    </div>
+    </Link>
   );
 }
