898c104b6aef9dbcb81927f2ce6c85c02c573cce
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-12-07 19:39:09 +0100

    add scroller to category container

--- frontend/src/components/categoryCard/CategoryCardContainer.tsx
+++ frontend/src/components/categoryCard/CategoryCardContainer.tsx
@@ -3,8 +3,10 @@ import { CategoryType } from "../../utils/types";
 import axios from "axios";
 import CategoryCard from "./CategoryCard";
 import style from "./CategoryCard.module.scss";
+import useWindowDimensions from "../../utils/hooks/useWindowDimensions";
 export default function CategoryCardContainer() {
   const [categories, setCategories] = useState<CategoryType[] | []>([]);
+  const windowSize = useWindowDimensions();
 
   useEffect(() => {
     axios
@@ -15,11 +17,36 @@ export default function CategoryCardContainer() {
       .catch((err) => console.error(err));
   }, []);
 
+  let x = 0;
+  const scroll = (direction: string) => {
+    const container = document.getElementsByClassName(
+      style.categoryCardContainer
+    );
+    if (direction === "right") {
+      x += 160;
+    } else {
+      x -= 160;
+    }
+    container[0].scrollTo({ left: x, behavior: "smooth" });
+  };
+
+  const ContainerSize = {
+    width: 166 * (Math.round((windowSize - 160) / 150) - 1),
+  };
+
   return (
-    <div className={style.categoryCardContainer}>
-      {categories.map((category) => (
-        <CategoryCard category={category} key={category.id} />
-      ))}
-    </div>
+    <>
+      <div className={style.categoryCardContainer} style={ContainerSize}>
+        {categories.map((category) => (
+          <CategoryCard category={category} key={category.id} />
+        ))}
+      </div>
+      <button type="button" onClick={() => scroll("left")}>
+        gauche
+      </button>
+      <button type="button" onClick={() => scroll("right")}>
+        droite
+      </button>
+    </>
   );
 }
