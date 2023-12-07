11485b4cc0cb20010943e30e27de4fd533089be6
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-12-07 21:47:53 +0100

    add buttons on left and right

--- frontend/src/components/categoryCard/CategoryCardContainer.tsx
+++ frontend/src/components/categoryCard/CategoryCardContainer.tsx
@@ -4,9 +4,16 @@ import axios from "axios";
 import CategoryCard from "./CategoryCard";
 import style from "./CategoryCard.module.scss";
 import useWindowDimensions from "../../utils/hooks/useWindowDimensions";
+import { ArrowLeftCircle, ArrowRightCircle } from "react-feather";
 export default function CategoryCardContainer() {
   const [categories, setCategories] = useState<CategoryType[] | []>([]);
   const windowSize = useWindowDimensions();
+  const containerSize = 166 * (Math.round((windowSize - 160) / 150) - 1);
+  const categoryCards = document.getElementsByClassName(style.categoryCard);
+  const container = document.getElementsByClassName(
+    style.categoryCardContainer
+  );
+  let x = 0;
 
   useEffect(() => {
     axios
@@ -17,36 +24,46 @@ export default function CategoryCardContainer() {
       .catch((err) => console.error(err));
   }, []);
 
-  let x = 0;
   const scroll = (direction: string) => {
-    const container = document.getElementsByClassName(
-      style.categoryCardContainer
-    );
     if (direction === "right") {
-      x += 160;
+      categoryCards[categoryCards.length - 1].getBoundingClientRect().x + 160 >
+      windowSize
+        ? (x += 165)
+        : x;
     } else {
-      x -= 160;
+      categoryCards[0].getBoundingClientRect().x < 80 ? (x -= 160) : 5 * 16;
     }
     container[0].scrollTo({ left: x, behavior: "smooth" });
   };
 
-  const ContainerSize = {
-    width: 166 * (Math.round((windowSize - 160) / 150) - 1),
-  };
-
   return (
     <>
-      <div className={style.categoryCardContainer} style={ContainerSize}>
-        {categories.map((category) => (
-          <CategoryCard category={category} key={category.id} />
-        ))}
+      <div className={style.categoryContainer}>
+        <div
+          className={style.categoryCardContainer}
+          style={{ width: containerSize }}
+        >
+          {categories.map((category) => (
+            <CategoryCard category={category} key={category.id} />
+          ))}
+        </div>
+
+        <button
+          type="button"
+          onClick={() => scroll("left")}
+          className={style.buttonLeft}
+        >
+          <ArrowLeftCircle />
+        </button>
+
+        <button
+          type="button"
+          onClick={() => scroll("right")}
+          className={style.buttonRight}
+        >
+          <ArrowRightCircle />
+        </button>
       </div>
-      <button type="button" onClick={() => scroll("left")}>
-        gauche
-      </button>
-      <button type="button" onClick={() => scroll("right")}>
-        droite
-      </button>
     </>
   );
 }
