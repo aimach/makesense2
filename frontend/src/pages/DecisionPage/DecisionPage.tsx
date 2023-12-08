57cff4dbc93ab1cbbed42dd049033af82dbdf021
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-12-08 08:37:24 +0100

    add fetching decision

--- frontend/src/pages/DecisionPage/DecisionPage.tsx
+++ frontend/src/pages/DecisionPage/DecisionPage.tsx
@@ -1,9 +1,28 @@
 import { useParams } from "react-router-dom";
 import style from "./DecisionPage.module.scss";
+import { useEffect, useState } from "react";
+import { DecisionType } from "../../utils/types";
+import axios from "axios";
 
 export default function DecisionPage() {
   const { decisionId } = useParams();
-  console.log(decisionId);
+  const [decision, setDecision] = useState<DecisionType | null>(null);
+
+  useEffect(() => {
+    axios
+      .get<DecisionType>(
+        `${import.meta.env.VITE_BACKEND_URL as string}/decisions/${
+          decisionId as string
+        }`
+      )
+      .then((res) => {
+        setDecision(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, [decisionId]);
+
+  console.log(decision);
+
   return (
     <div className={style.decisionPageContainer}>
       <section></section>
